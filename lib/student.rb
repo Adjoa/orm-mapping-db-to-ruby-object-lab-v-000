0e3866d6638a9b03971c8fa8acca3c290ef94e30
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-06 19:00:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,12 @@ class Student
   end
 
   def first_X_students_in_grade_10
-
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade = ?
+      LIMIT ?
+    SQL
   end
 
 
