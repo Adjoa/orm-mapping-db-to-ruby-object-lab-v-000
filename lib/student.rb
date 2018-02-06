d478c21f6c445568c0ce7d8e935a815a5496fe37
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-06 19:09:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -96,7 +96,8 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(1).new_from_db
+    first_student_row = self.first_X_students_in_grade_10(1
+    self.new_from_db(first_student_row)
   end
 
 end
